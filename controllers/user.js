07df707d94b20e50dad83847c7c9cc4f546da8f4
Arbenisya <arbenisya@gmail.com>
2023-11-14 05:40:45 +0700

    video 15

--- controllers/user.js
+++ controllers/user.js
@@ -38,22 +38,25 @@ module.exports = {
           res.status(400).json({success: false})
         }
       },
-      update:  (req, res) => {
+      update:  async(req, res) => {
+        try {
+          const user = await User.findByIdAndUpdate(req.params.id, req.body,{
+              new: true,
+              runValidators: true
+          })
+          res.json({
+            status: true,
+            data: user,
+            method: req.method,
+            url: req.url,
+            message: "Data berhasil diubah"
+          })
+          
+        } catch (error) {
+          res.status(400).json({success: false})
+        }
         const id = req.params.id
-        users.filter(user => {
-            if(user.id == id){
-                user.nama = req.body.nama
-                user.email = req.body.email
-                return user
-            }
-        })
-        res.json({
-          status: true,
-          data: users,
-          method: req.method,
-          url: req.url,
-          message: "Data berhasil diubah"
-        })
+
       },
       delete: (req, res) => {
         const id = req.params.id
